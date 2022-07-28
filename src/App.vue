e3d9b277b915fb66febc4363edee7067033af114
yranky <douye@douye.top>
2022-07-28 22:42:57 +0800

    feat:用户中心

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-
+	
 </script>
 <style>
 	/* #ifdef APP-NVUE */
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,13 @@
 export const theme = {
- 	// 这里输入你定义的主题主色
+	// 这里输入你定义的主题主色
 	//比如："primary": "#FF0000"
 	//名称如果与自带主题相同，将会覆盖。
-	"primary":'#40a9ff',
-	"whiteBg":"#fff",
-	"imputBg":"rgba(255,255,255,.2)",
-	"primary2":"#ff9c3f",
-	"primary3":"#597ffa",
-	"primary4":"#4dfa78"
- }
+	"primary": '#40a9ff',
+	"whiteBg": "#fff",
+	"imputBg": "rgba(255,255,255,.2)",
+	"primary2": "#ff9c3f",
+	"primary3": "#597ffa",
+	"primary4": "#4dfa78",
+	"vip": "#edc387",
+	"vipBg":"#2b3840"
+}
