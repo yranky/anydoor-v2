2771d9b51ca75631b69a35ca12d756040cd52718
yranky <douye@douye.top>
2023-02-06 14:51:52 +0800

    chore:常量名称修改

---
+++ src/common/database/swiper/Swiper.ts
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 轮播图数据处理获取部分等
+ */
+
+export default class Swiper {
+
+}
--- src/common/network/NETError.ts
+++ src/common/network/NETError.ts
@@ -1,24 +1,24 @@
-
-export enum status {
-	empty = "empty",
-	error = "error",
-	success = "success",
-	warning = "warning",
-	lock = "lock",
-	network = "network"
+//状态
+export enum STATUS {
+	EMPTY = "empty",
+	ERROR = "error",
+	SUCCESS = "success",
+	WARNING = "warning",
+	LOCK = "lock",
+	NETWORK = "network"
 }
 
-export enum action {
-	retry = "重试",
-	default = ""
+export enum ACTION {
+	RETRY = "重试",
+	DEFAULT = ""
 }
-
+//网络错误
 export default interface NETError {
 	name: string,
 	show: boolean,
 	color: string,
-	status: status,
-	btnText: action,
+	status: STATUS,
+	btnText: ACTION,
 	title: string,
 	subTitle: string,
 	showBtn: boolean,
