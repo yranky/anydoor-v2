2f365600722c4989ed7509492171cf6c0a77c7e5
yranky <douye@douye.top>
2023-04-15 18:56:32 +0800

    feat:diaolog模块

--- src/common/native/dialog/DialogModule.ts
+++ src/common/native/dialog/DialogModule.ts
@@ -4,6 +4,8 @@ export default class DialogModule {
     static getInstance(): DialogModule {
         if (uni.$anydoor.DialogModule === undefined) {
             uni.$anydoor.DialogModule = new DialogModule()
+            //初始化
+            uni.$anydoor_native.Dialog_Module.init({});
         }
         return uni.$anydoor.DialogModule
     }
